0666da75ca889307d576d98fd8d56a8c44d0ed9f
Harisharan Mishra <131943689+HarisharanM@users.noreply.github.com>
2025-08-22 16:03:22 +0530

    Add files via upload

---
+++ Assignment Anudip/src/com/Hari/anu/Rectangle.java
@@ -0,0 +1,17 @@
+package com.Hari.anu;
+
+public class Rectangle {
+    double width;
+    double height;
+
+    enum Color {
+        RED, GREEN, BLUE
+    }
+
+    Color boxColor;
+
+    public static void main(String[] args) {
+        System.out.println(Color.BLUE);
+    }
+}
+
